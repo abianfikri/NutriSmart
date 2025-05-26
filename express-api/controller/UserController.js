22a1453908c39d879e49bbf3dfd32353fa9e63bc
Abian Fikri <boyanonim22@gmail.com>
2025-05-26 09:05:44 +0700

    done => menambahkan get profile by token

--- express-api/controller/UserController.js
+++ express-api/controller/UserController.js
@@ -28,6 +28,64 @@ const getUsers = async (req, res) => {
     }
 };
 
+const getUserProfileByToken = async (req, res) => {
+    try {
+        const user = await Users.findOne({
+            where: {
+                id: req.userId
+            },
+            attributes: {
+                exclude: ["password", "refresh_token", "createdAt", "updatedAt"]
+            }
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "Profile fetched successfully",
+            data: user
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+const updateProfile = async (req, res) => {
+    try {
+        const user = await Users.findByPk(req.userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const { name, email } = req.body;
+        user.name = name;
+        user.email = email;
+
+        await user.save();
+
+        res.status(200).json({
+            status: "success",
+            message: "Profile updated successfully",
+            data: {
+                id: user.id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+
 export default {
     getUsers,
+    getUserProfileByToken, // tambahkan ini
+    updateProfile
 };
+
