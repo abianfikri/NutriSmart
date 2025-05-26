998d3fa9991fc83af6b9edcf551b935bd0e1a36c
Abian Fikri <boyanonim22@gmail.com>
2025-05-26 09:06:37 +0700

    done => menambahkan route profile user

--- express-api/routes/userRoute.js
+++ express-api/routes/userRoute.js
@@ -4,5 +4,7 @@ import userController from '../controller/UserController.js';
 const router = express.Router();
 
 router.get('/', userController.getUsers);
+router.get('/me', userController.getUserProfileByToken);
+router.patch('/me', userController.updateProfile);
 
 export default router;
