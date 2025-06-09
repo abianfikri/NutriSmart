03928521de3d43720ea6497f98bfcc0c82bd29d9
Abian Fikri <boyanonim22@gmail.com>
2025-06-09 13:12:54 +0700

    refactor => simpan gambar http edamam ke dalam firebase

--- express-api/service/MealPlanService.js
+++ express-api/service/MealPlanService.js
@@ -1,5 +1,6 @@
 import Meals from '../models/MealModel.js';
 import MealItems from '../models/MealItemModel.js';
+import { uploadImageToFirebase } from '../helper/FirebaseUpload.js';
 
 export async function saveMealPlanToDb(mealPlan, userId) {
     for (const day of mealPlan) {
@@ -10,11 +11,21 @@ export async function saveMealPlanToDb(mealPlan, userId) {
 
         for (const mealType of ['Breakfast', 'Lunch', 'Dinner']) {
             const item = day.meals[mealType];
+
+            let firebaseImageUrl = null;
+            if (item.image) {
+                try {
+                    const filename = `meal-images/${Date.now()}-${day.day}-${mealType}.jpg`;
+                    firebaseImageUrl = await uploadImageToFirebase(item.image, filename);
+                } catch (error) {
+                    console.error('Error uploading image to Firebase:', error);
+                }
+            }
             await MealItems.create({
                 mealId: meal.id,
                 mealType,
                 label: item.label,
-                imageUrl: item.image, // filename / URL yang sudah dari Firebase
+                imageUrl: firebaseImageUrl, // filename / URL yang sudah dari Firebase
                 calories: item.calories,
                 protein: item.protein,
                 fat: item.fat,
