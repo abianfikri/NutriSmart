02653fbab78dbe3726040a0ac6f6f460b920fcb1
Abian Fikri <boyanonim22@gmail.com>
2025-06-14 22:03:00 +0700

    refactor => rename path service api

--- express-api/routes/userRoute.js
+++ express-api/routes/userRoute.js
@@ -7,7 +7,9 @@ const router = express.Router();
 router.get('/', userController.getUsers);
 router.get('/me', userController.getUserProfileByToken);
 router.get('/analysis', userController.calculateTDDE);
-router.post('/meal-plan', FoodController.getMealPlan);
+router.get('/meal-plan', FoodController.getSaveMealPlan);
+
+router.post('/recommendation', FoodController.getMealPlan);
 router.post('/meal-plan/save', FoodController.saveMealPlan);
 router.patch('/me', userController.updateProfile);
 
